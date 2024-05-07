ff7484de1acfb31bb0d3ad6e29b15f57466a9885
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-07 12:04:33 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
-				<h1>Coming Soon...</h1>
-			</div>
-		</main>
-	);
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Coming Soon...</h1>
+      </div>
+    </main>
+  );
 }
