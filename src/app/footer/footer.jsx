2578edd1ce66698f9e7b60839a652ef277b7ea39
Emma Nolan <emmanolan@Emmas-Mac-mini.local>
2024-05-08 16:29:23 +0100

    added footer text and styling

--- src/app/footer/footer.jsx
+++ src/app/footer/footer.jsx
@@ -3,7 +3,11 @@ import "./footer.css";
 export default function Footer() {
   return (
     <>
-      <h1 className="footerh1">Footer</h1>
+      <footer>
+        <p>
+          Powered by <b> Relabel</b>
+        </p>
+      </footer>
     </>
   );
 }
