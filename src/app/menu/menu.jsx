1b646f7ac9614699edb6a2eb1a58f70e213e2341
Gavin-Persad <gavinapersad@gmail.com>
2024-05-08 12:29:59 +0100

    care instuctions map working

--- src/app/menu/menu.jsx
+++ src/app/menu/menu.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Accordion } from "react-bootstrap";
 import Materials from "./materials/materials";
+import CareInstructions from "./careInstructions/careInstructions";
 
 export default function Menu({ data }) {
   return (
@@ -16,15 +17,9 @@ export default function Menu({ data }) {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>Care Instructions</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+           <CareInstructions careInstructions={data.careInstructions}/>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,33 @@
-import styles from "./page.module.css";
-import Header from "./header/header";
-import ProductCard from "./productCard/productCard";
-import Menu from "./menu/menu";
-import Footer from "./footer/footer";
+import styles from './page.module.css';
+import Header from './header/header';
+import ProductCard from './productCard/productCard';
+import Menu from './menu/menu';
+import Footer from './footer/footer';
 
 export default function Home() {
-  let menuData = {
-    materials: ["cotton", "wool"],
-  };
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <Header />
-        <ProductCard
-          productImage={"exampleImageUrl"}
-          productName={"exampleName"}
-          location={"exampleLocation"}
-          highlights={["exampleHightLight"]}
-        />
-        <Menu data={menuData} />
-        <Footer />
-      </div>
-    </main>
-  );
+	let menuData = {
+		materials: ['cotton', 'wool'],
+		careInstructions: [
+			{
+				image: 'exampleImageUrl',
+				instruction: 'exampleInstruction',
+			},
+		],
+	};
+
+	return (
+		<main className={styles.main}>
+			<div className={styles.description}>
+				<Header />
+				<ProductCard
+					productImage={'exampleImageUrl'}
+					productName={'exampleName'}
+					location={'exampleLocation'}
+					highlights={['exampleHightLight']}
+				/>
+				<Menu data={menuData} />
+				<Footer />
+			</div>
+		</main>
+	);
 }
