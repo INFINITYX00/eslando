ae8a9574d7a78695b02ec555dc981df28a9f0c4a
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-08 16:54:16 +0100

    data fully integrated

--- src/app/menu/materials/carouselCard/carouselCard.jsx
+++ src/app/menu/materials/carouselCard/carouselCard.jsx
@@ -3,8 +3,17 @@ import "./carouselCard.css";
 export default function CarouselCard({ material }) {
   return (
     <div className="carouselCard">
-      <p>{material.name}</p>
-      <p>{material.material}</p>
+      {material.map((item, index) => (
+        <div key={index}>
+          {Object.entries(item).map(([key, value]) => (
+            <div key={key}>
+              <p>
+                {key}:{value}
+              </p>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
