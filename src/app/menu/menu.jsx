4391dcbb10f9bf6bdb39498a2d6344408e7f9880
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-08 10:19:38 +0100

    Carousel added to materials component

--- src/app/menu/menu.jsx
+++ src/app/menu/menu.jsx
@@ -3,21 +3,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Accordion } from "react-bootstrap";
+import Materials from "./materials/materials";
 
 export default function Menu() {
   return (
     <>
-       <Accordion defaultActiveKey="0">
+      <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>Materials</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <Materials />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
---
+++ src/app/title/title.jsx
@@ -0,0 +1,3 @@
+export default function Title({ titleText }) {
+  return <h2>{titleText}</h2>;
+}
