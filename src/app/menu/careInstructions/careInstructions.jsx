bcc8fb0070656d421a16225ec35ec5d6f72ca05a
Stephen <scpwalker@hotmail.com>
2024-05-09 13:23:53 +0100

    changed title

--- src/app/menu/careInstructions/careInstructions.jsx
+++ src/app/menu/careInstructions/careInstructions.jsx
@@ -1,17 +1,17 @@
 import Title from "../title/title";
 
 export default function CareInstructions({ careInstructions }) {
-    return (
-        <>
-        <Title title="Care Instructions" />
-        <ul>
-            {careInstructions.map((instruction) => (
-            <li key={instruction.instruction}>
-                <img src={instruction.image} />
-                <p>{instruction.instruction}</p>
-            </li>
-            ))}
-        </ul>
-        </>
-    );
+  return (
+    <>
+      <Title title="Care Instructions!" />
+      <ul>
+        {careInstructions.map((instruction) => (
+          <li key={instruction.instruction}>
+            <img src={instruction.image} />
+            <p>{instruction.instruction}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
