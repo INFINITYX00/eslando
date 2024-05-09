bf704b1d6e8d7105aafb35488852b3a1ef44bcd5
Stephen <145766101+INFINITY00users.noreplygithub.com>
2024-05-09 09:51:33 +0100

    header css module added

--- src/app/header/header.jsx
+++ src/app/header/header.jsx
@@ -1,11 +1,11 @@
-import "./header.css";
+import HeaderCSS from "./header.module.css";
 
 export default function Header() {
   return (
     <>
-      <header>
+      <header className={HeaderCSS.header}>
         <img
-          className="dpp-icon"
+          className={HeaderCSS.dppIcon}
           src="./dpp-icon.png"
           alt="A simple black passport icon with a globe on the front."
         />
