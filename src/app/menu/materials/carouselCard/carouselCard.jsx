9ce526b8659a30a8e33d2c5e0f4ea08f52bd7049
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-09 14:21:56 +0100

    styled carousel card

--- src/app/menu/materials/carouselCard/carouselCard.jsx
+++ src/app/menu/materials/carouselCard/carouselCard.jsx
@@ -3,22 +3,23 @@ import "./carouselCard.css";
 export default function CarouselCard({ material }) {
   return (
     <div className="carouselCard">
-      {material.map((item, index) => (
-        <div className="cardDiv" key={index}>
-          {Object.entries(item).map(([key, value]) =>{
-            if (key == "name"){
-              return <h1 key={key}>{value}</h1>
-            }
-            else{
-            return ( 
-            <div key={key}>
-              <p>
-                {value}
-              </p>
-            </div>
-          )}})}
-        </div>
-      ))}
+      <div className="borderbox">
+        {material.map((item, index) => (
+          <div className="cardDiv" key={index}>
+            {Object.entries(item).map(([key, value]) => {
+              if (key == "name") {
+                return <h1 key={key}>{value}</h1>;
+              } else {
+                return (
+                  <div key={key}>
+                    <p>{value}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
