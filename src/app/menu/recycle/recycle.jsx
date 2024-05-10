5dd8c45e2e930fdd4c43afce6c53cfc919222b39
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-10 09:47:09 +0100

    basic input field styling

--- src/app/menu/recycle/recycle.jsx
+++ src/app/menu/recycle/recycle.jsx
@@ -1,34 +1,36 @@
-import Title from "../title/title";
 import { useState } from "react";
+import "./recycle.css";
 
 export default function Recycle({ recycle }) {
-    const [displayShops, setDisplayShops] = useState(false)
+  const [displayShops, setDisplayShops] = useState(false);
 
-    const handleSumbit = (e) => {
-        e.preventDefault()
-        setDisplayShops(true)
-    }
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    setDisplayShops(true);
+  };
 
-    return (
-        <>
-        <Title title="Recycle and Repurpose" />
-        <form onSubmit={handleSumbit}>
-            <input placeholder="Enter Postcode"/>
-            <button type="submit">Search for locations</button>
-        </form>
-        {displayShops && (
-            <div>
-                <ul>
-                    {recycle.map((shop) => (
-                        <li key={shop.name}>
-                            <h3>{shop.name}</h3>
-                            <p>{shop.address}</p>
-                            <p>{shop.phone}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )}
-        </>
-    )
+  return (
+    <>
+      <form className="form" onSubmit={handleSumbit}>
+        <h3 className="recycleHeading">♻️ Find Your Local Recycling Shop</h3>
+        <div className="inputField">
+          <input placeholder="Enter Postcode" />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+      {displayShops && (
+        <div>
+          <ul>
+            {recycle.map((shop) => (
+              <li key={shop.name}>
+                <h3>{shop.name}</h3>
+                <p>{shop.address}</p>
+                <p>{shop.phone}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
