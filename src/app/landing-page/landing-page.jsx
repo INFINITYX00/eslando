d97a80cb908229f3c4c65f2dffda634e0488dd06
Emma Nolan <emmanolanx@hotmail.com>
2024-05-13 14:54:16 +0100

    added background image and text to landing page

--- src/app/landing-page/landing-page.jsx
+++ src/app/landing-page/landing-page.jsx
@@ -31,14 +31,32 @@ export default function LandingPage() {
   // Return the landing page component
 
   return (
-    <div
-      className={
-        showLandingPage
-          ? LandingCSS.landingWrapper
-          : `${LandingCSS.landingWrapper} ${LandingCSS.hidden}`
-      }
-    >
-      <h1 className={LandingCSS.test}>Hello this is a test</h1>
-    </div>
+    <>
+      <div
+        className={
+          showLandingPage
+            ? LandingCSS.landingBackground
+            : `${LandingCSS.landingBackground} ${LandingCSS.hidden}`
+        }
+      >
+        <div className={LandingCSS.header}>
+          <h1>RELABEL</h1>
+        </div>
+        <div className={LandingCSS.landingText}>
+          <p>
+            LET'S FIND OUT MORE ABOUT YOUR
+            <br />
+            <b>FLEECE DENIM JACKET</b> <br />
+            FROM
+            <br />
+            <b>DENIMOLOGY</b>
+          </p>
+        </div>
+        <div className={LandingCSS.landingScroll}>
+          <p>SCROLL DOWN</p>
+          <img src="arrow-down-svgrepo-com.svg" />
+        </div>
+      </div>
+    </>
   );
 }
