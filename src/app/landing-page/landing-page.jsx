d965121073a92cb1484aa4f7a4b4b6d310a7efcc
Stephen <scpwalker@hotmail.com>
2024-05-13 12:29:58 +0100

    added landing page scroll feature that disappears when main page displayed

--- src/app/landing-page/landing-page.jsx
+++ src/app/landing-page/landing-page.jsx
@@ -1,11 +1,34 @@
+"use client";
+import { useState, useEffect } from "react";
 import LandingCSS from "./landing-page.module.css";
 
 export default function LandingPage() {
+  const [showLandingPage, setShowLandingPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check if the user has scrolled down the page
+      if (window.scrollY > 0) {
+        setShowLandingPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-    <div className={LandingCSS.landingWrapper}>
+    <div
+      className={
+        showLandingPage
+          ? LandingCSS.landingWrapper
+          : `${LandingCSS.landingWrapper} ${LandingCSS.hidden}`
+      }
+    >
       <h1 className={LandingCSS.test}>Hello this is a test</h1>
-      </div>
-    </>
+    </div>
   );
 }
