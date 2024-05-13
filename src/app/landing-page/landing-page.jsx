e21ff402739ac09d1b234886dc614b2832fe0343
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-05-13 15:08:36 +0100

    changed background image from css to jsx

--- src/app/landing-page/landing-page.jsx
+++ src/app/landing-page/landing-page.jsx
@@ -39,12 +39,14 @@ export default function LandingPage() {
             : `${LandingCSS.landingBackground} ${LandingCSS.hidden}`
         }
       >
+        <img className={LandingCSS.bgImage} src="https://images.unsplash.com/photo-1574201635302-388dd92a4c3f?q=80&w=3284&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)" alt="Clothing image"></img>
+
         <div className={LandingCSS.header}>
           <h1>RELABEL</h1>
         </div>
         <div className={LandingCSS.landingText}>
           <p>
-            LET'S FIND OUT MORE ABOUT YOUR
+            LET&apos;S FIND OUT MORE ABOUT YOUR
             <br />
             <b>FLEECE DENIM JACKET</b> <br />
             FROM
@@ -56,7 +58,7 @@ export default function LandingPage() {
           <p>SCROLL DOWN</p>
           <img src="arrow-down-svgrepo-com.svg" />
         </div>
-      </div>
+        </div>
     </>
   );
 }
