50a1530c3362f0af521a853fa5a4d2eab951737a
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-05-13 11:51:26 +0100

    landing page takes up full screen

--- src/app/landing-page/landing-page.jsx
+++ src/app/landing-page/landing-page.jsx
@@ -3,7 +3,9 @@ import LandingCSS from "./landing-page.module.css";
 export default function LandingPage() {
   return (
     <>
+    <div className={LandingCSS.landingWrapper}>
       <h1 className={LandingCSS.test}>Hello this is a test</h1>
+      </div>
     </>
   );
 }
