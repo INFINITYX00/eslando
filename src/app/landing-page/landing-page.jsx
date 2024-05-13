48997303289c603bead663a3b3f445678baf3d9a
Stephen <scpwalker@hotmail.com>
2024-05-13 13:11:33 +0100

    code on landing page component commented

--- src/app/landing-page/landing-page.jsx
+++ src/app/landing-page/landing-page.jsx
@@ -2,23 +2,33 @@
 import { useState, useEffect } from "react";
 import LandingCSS from "./landing-page.module.css";
 
+// Landing page component
+// This component will be displayed when the user first visits the website
+// As the user scrolls down the page, the landing page will be hidden
+
 export default function LandingPage() {
+  // State to determine if the landing page should be displayed
   const [showLandingPage, setShowLandingPage] = useState(true);
 
+  // Event listener to check if the user has scrolled down the page
   useEffect(() => {
     const handleScroll = () => {
       // Check if the user has scrolled down the page
       if (window.scrollY > 0) {
+        // Hide the landing page
         setShowLandingPage(false);
       }
     };
-
+    // Add event listener to check if the user has scrolled down the page
     window.addEventListener("scroll", handleScroll);
 
+    // Remove event listener when the component is unmounted
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  // Return the landing page component
 
   return (
     <div
