fad800f4269d52ad0f631cc4fe04d9272c0d042c
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-15 09:43:10 +0100

    added suspense

--- src/app/page.js
+++ src/app/page.js
@@ -8,30 +8,33 @@ import Menu from "./menu/menu";
 import Footer from "./footer/footer";
 import productData from "../../productData";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Home() {
   const [searchParams] = useSearchParams();
   let data = productData[searchParams[1]].product;
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <div className={styles.landing}>
-          <LandingPage productName={data.name} brand={data.brand} />
+    <Suspense>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <div className={styles.landing}>
+            <LandingPage productName={data.name} brand={data.brand} />
+          </div>
+          <div className={styles.content}>
+            <Header />
+            <ProductCard
+              productImage={data.imageUrl}
+              productName={data.name}
+              brand={data.brand}
+              location={data.countryOfOrigin}
+              highlights={data.highlights}
+            />
+            <Menu data={data.menu} />
+            <Footer />
+          </div>
         </div>
-        <div className={styles.content}>
-          <Header />
-          <ProductCard
-            productImage={data.imageUrl}
-            productName={data.name}
-            brand={data.brand}
-            location={data.countryOfOrigin}
-            highlights={data.highlights}
-          />
-          <Menu data={data.menu} />
-          <Footer />
-        </div>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 }
