fdc8c79e0e01d89d53a50d1a5d15fd955ada4910
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-15 13:36:36 +0100

    added season in productCard

--- src/app/productCard/productCard.jsx
+++ src/app/productCard/productCard.jsx
@@ -1,30 +1,33 @@
 import ProductCSS from "./productCard.module.css";
 
 export default function ProductCard({
- productImage,
- productName,
- brand,
- location,
- highlights,
+  productImage,
+  productName,
+  brand,
+  season,
+  location,
+  highlights,
 }) {
- return (
-   <>
-     <img
-       className={ProductCSS.image}
-       src={productImage}
-       alt="Picture of clothes"
-     />
-     <h1 className={ProductCSS.productName}>{productName}</h1>
-     <h2 className={ProductCSS.productBrand}>{brand}</h2>
-     <h3 className={ProductCSS.productLocation}>Made in {location}</h3>
-     <div className={ProductCSS.highlightDiv}>
-       {highlights.map((highlight) => (
-         <div className={ProductCSS.highlightContainer} key={highlight.title}>
-           <img src={highlight.svg} alt="highlight icon" />
-           <p className={ProductCSS.pHighlightCard}>{highlight.title}</p>
-         </div>
-       ))}
-     </div>
-   </>
- );
+  return (
+    <>
+      <img
+        className={ProductCSS.image}
+        src={productImage}
+        alt="Picture of clothes"
+      />
+      <h1 className={ProductCSS.productName}>{productName}</h1>
+      <h2 className={ProductCSS.productBrand}>
+        {brand}, {season}
+      </h2>
+      <h3 className={ProductCSS.productLocation}>Made in {location}</h3>
+      <div className={ProductCSS.highlightDiv}>
+        {highlights.map((highlight) => (
+          <div className={ProductCSS.highlightContainer} key={highlight.title}>
+            <img src={highlight.svg} alt="highlight icon" />
+            <p className={ProductCSS.pHighlightCard}>{highlight.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
