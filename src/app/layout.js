11dd005e401e290dcbdbfec2493275751e2cf75e
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-15 15:34:05 +0100

    suspense added to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
