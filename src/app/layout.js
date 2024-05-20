326cc5f6c44d02fb4b4bf00e3297acbcd9270391
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-20 14:14:36 +0100

    removed second body

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Suspense>
         <body className={inter.className}>{children}</body>
-        <body className={inter.className}>{children}</body>
       </Suspense>
     </html>
   );
