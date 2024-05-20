291b199e48c2f303dfc3b70dd9b1d005d12dd052
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-20 14:11:51 +0100

    readded suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
