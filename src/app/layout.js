072ad8ca8b50f4d0f9c363eb64a65170566514ad
Stephen <scpwalker@hotmail.com>
2024-05-20 11:54:52 +0100

    suspense added in before merge

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
