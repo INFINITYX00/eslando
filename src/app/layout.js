ea9d7824837cbfc17b4534af11525625536c7233
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-20 14:07:25 +0100

    initial fade in for second page

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Suspense>
-        <body className={inter.className}>{children}</body>
-        <body className={inter.className}>{children}</body>
-      </Suspense>
+      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
