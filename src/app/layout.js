7d692669b68000889016c8038df899e43af5bee2
Stephen <scpwalker@hotmail.com>
2024-05-21 11:12:41 +0100

    suspense tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,24 @@
-import { Open_Sans } from 'next/font/google';
-import './globals.css';
-import { Suspense } from 'react';
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { Suspense } from "react";
 
-const inter = Open_Sans({ weight: '400', subsets: ['latin'] });
+const inter = Open_Sans({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
-	title: 'Digital Product Passport',
-	description: 'Discover more about your clothing.',
+  title: "Digital Product Passport",
+  description: "Discover more about your clothing.",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<link
-				rel="stylesheet"
-				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
+      <Suspense>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
+    </html>
+  );
 }
