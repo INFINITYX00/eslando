e9981b15c6e5d14932f5fd0be32cb44787f81177
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-21 12:18:20 +0100

    readded suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,9 @@ export default function RootLayout({ children }) {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
